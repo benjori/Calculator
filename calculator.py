0176e53b98fe513c7b0bc4bdf36a9fc730b827fc
bronathan <22755351+benjori@users.noreply.github.com>
2023-10-13 00:14:48 -0400

    step 4 is complete

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
-operation = input('''
+# Define our function
+def calculate():
+    operation = input('''
 Please type in the math opperation you would like to complete:
 + for addition
 - for subtraction
@@ -6,24 +8,51 @@ Please type in the math opperation you would like to complete:
 / for division
 ''')
 
-number_1 = int(input('Enter your first number '))
-number_2 = int(input('Enter your second number: '))
+    number_1 = int(input('Enter your first number '))
+    number_2 = int(input('Enter your second number: '))
 
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
 
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
 
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
 
-else:
-    print('You have not typed a valid opperator, please run the program again.')
+    else:
+        print('You have not typed a valid opperator, please run the program again.')
+
+    # Add again() function to calculate() function
+    again()
+
+# Define again() function to ask user if they want to use the calculator again
+def again():
+
+    # Take input from user
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for Yes or N for No.
+''')
+
+    # If user types Y, run the calculate() function
+    if calc_again.upper() == 'Y':
+        calculate()
+
+    # If user types N, say good-bye to the user and end the program
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+
+    # If user types another key, run the function again
+    else:
+        again()
+
+# Call calculate() outside of the function
+calculate()
